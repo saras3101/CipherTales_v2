e425530b38420e3bf4625f06b2cd0d2b8189b562
saras3101 <codeitsaras31@gmail.com>
2025-11-01 15:01:28 +0530

    change url 1

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,2 +1,2 @@
 
-export const API_BASE_URL = "/choreo-apis/ciphertales/backend/v1/api"
+export const API_BASE_URL = "https://504ca22d-85be-4b97-b718-e75e82e094bf-dev.e1-us-east-azure.choreoapis.dev/api"
